71e26fa3566f524869b4199397f2cebc63b39ac9
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2020-11-27 20:09:25 -0500

    commit tree 3dd68f29af23cc94b1745becc53cad6ebddd2a7f

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,37 @@
+class CashRegister
+    attr_accessor :discount, :total, :items, :most_recent
+
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+        @items = []
+    end
+
+    def total=(total)
+        @total = total*(1-(@discount/100))
+    end
+
+    def add_item(title, price, quantity = 1)
+        self.total += price * quantity
+        i = 0
+        while i < quantity
+            @items << title
+            i += 1
+        end
+        self.most_recent = price * quantity
+    end
+
+    def apply_discount
+        if @discount > 0
+            self.total = (@total*((100.0-@discount)/100.0)).to_i
+            "After the discount, the total comes to $#{self.total}."
+        else
+            "There is no discount to apply."
+        end
+    end
+    
+    def void_last_transaction
+        self.total = self.total - self.most_recent
+    end
+end
 
